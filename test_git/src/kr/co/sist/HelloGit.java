55ff48dc71b9cd8a2f09f99fec74b2345ac7dc0b
sgmsgood <45481968+sgmsgood@users.noreply.github.com>
2019-02-13 16:30:59 +0900

    두번째 연습

--- test_git/src/kr/co/sist/HelloGit.java
+++ test_git/src/kr/co/sist/HelloGit.java
@@ -1,5 +1,8 @@
 package kr.co.sist;
 
 public class HelloGit {
-
+	public static void main(String[] args) {
+		System.out.println("오늘은 갑자기 어깨가 결린다.");
+		System.out.println("오늘은 갑자기 어깨가 결린다.");
+	}
 }
